f8f20aeb632b7b44df1b998bbbd6edbb32db242d
matt oballe <oballematt@gmail.com>
2022-08-15 21:00:16 -0500

    initial commit, setting up react app

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Titlie</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
