e45a0e59001909368d12320b1966e0129e6ecdaa
matt oballe <oballematt@gmail.com>
2022-08-15 21:34:25 -0500

    creating props for ExpenseItem Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+const expenseDate = new Date(2022, 9, 15);
+const expenseTitle = 'Car Insurance'
+const expenseAmount = 294.67
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{props.date}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
